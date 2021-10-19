757da92e35fdc57091d66cfdc513ec465c88e00f
Shivang <iamshivang@outlook.com>
2021-10-20 02:10:28 +0530

    ver2.0.0

---
+++ Day_1/introduction/src/components/Button.js
@@ -0,0 +1,23 @@
+import "./button.css"
+
+function Button(){
+ //{age:123,name:"hello"}
+ return (
+   <div id="a">
+     <div id='b'>
+       <div>JOIN US</div>
+       <div>LOGIN</div>
+       <div>SEARCH</div>
+       <div>HOME</div>
+     </div>
+     <div id="c">
+       <div>SETTINGS</div>
+       <div>CONTACT US</div>
+       <div>HELP</div>
+       <div>DOWNLOAD</div>
+     </div>
+   </div>
+ )
+}
+
+export {Button}
---
+++ Day_1/mobile_os/src/App.js
@@ -0,0 +1,23 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Mobile Operating Systems</h1>
+      <ul>
+        <li>Android</li>
+        <li>BlackBerry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </ul>
+      <h1>Mobile Manufacturer</h1>
+      <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
